b7189327bc239814237f749330fa3750004ad534
Shamil <badalloffsh@gmail.com>
2016-12-14 01:16:37 +0400

    modify

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :user, 'deployer'
 append :linked_files, 'config/database.yml', '.env'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor/bundle', 'public/uploads'
+append :linked_dirs, 'bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/uploads'
 
 namespace :deploy do
   %w[start stop restart].each do |command|
