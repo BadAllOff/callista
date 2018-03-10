1ed903922ae47406cc8deeb733c84e649304a0c2
Shamil <badalloffsh@gmail.com>
2018-03-10 13:33:26 +0100

    Add: toggle side admin menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,6 +39,18 @@ ready = function() {
             }
         }
     });
+
+    $('#slide-submenu').on('click',function() {
+        $(this).closest('.list-group').fadeOut('slide',function(){
+            $('.mini-submenu').fadeIn();
+        });
+
+    });
+
+    $('.mini-submenu').on('click',function(){
+        $(this).next('.list-group').toggle('slide');
+        $('.mini-submenu').hide();
+    })
 };
 
 $(document).ready(ready);
